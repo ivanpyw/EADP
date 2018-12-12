fcb43f01f0ac60803cd75a0c6ad80fdda85ea8ed
Aaron <170313q@mymail.nyp.edu.sg>
2018-12-12 18:47:54 +0800

    select triptitle nia

--- eadLab5/TripDetails.aspx.cs
+++ eadLab5/TripDetails.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using eadLab5.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,14 @@ namespace eadLab5
 {
     public partial class TripDetails : System.Web.UI.Page
     {
+
+        protected int count = 0;
+        protected List<Trip> tripObj = null;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            TripDAO tripDao = new TripDAO();   
+            tripObj = tripDao.getTrip();
+            count = tripDao.count;
         }
     }
 }
